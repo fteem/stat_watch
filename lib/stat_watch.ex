5a94a214250d2b4bfe43323923c201bde37bfcf6
Ilija Eftimov <ilija@scribd.com>
2019-11-08 18:43:00 +0100

    Add StatWatch.run that fetches stats and saves them to CSV

--- lib/stat_watch.ex
+++ lib/stat_watch.ex
@@ -1,2 +1,22 @@
 defmodule StatWatch do
+  alias StatWatch.Youtube
+
+  def run do
+    Youtube.fetch_stats()
+    |> save_csv()
+  end
+
+  def column_names do
+    Enum.join(~w(DateTime Subscribers Videos Views), ",")
+  end
+
+  def save_csv(row) do
+    filename = "stats.csv"
+
+    unless File.exists?(filename) do
+      File.write!(filename, column_names() <> "\n")
+    end
+
+    File.write!(filename, row <> "\n", [:append])
+  end
 end
